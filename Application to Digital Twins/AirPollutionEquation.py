2ca1eb80dff803ea435bc14cf6c82b742d37246d
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2024-02-02 17:55:11 +0100

    Update AirPollutionEquation.py

--- Application to Digital Twins/AirPollutionEquation.py
+++ Application to Digital Twins/AirPollutionEquation.py
@@ -6,17 +6,18 @@ import numpy as np
 mesh = RectangleMesh(Point(-5,-4), Point(5, 4), 200, 200, diagonal = "crossed")
 
 V = FunctionSpace(mesh, 'CG', 1)
-FOMspace = V
+cmesh = RectangleMesh(Point(-5,-4), Point(5, 4), 80, 80, diagonal = "crossed")
+FOMspace = FunctionSpace(cmesh, 'CG', 1)
 
 def FOMsolver(d1, d2, d3, d4, theta):
   v = TestFunction(V)
   u = Function(V)
   w1, w2, w3, w4 = Function(V), Function(V), Function(V), Function(V)
 
-  I1 = Expression('(pow(x[0] + 2, 2) + pow(x[1] + 2.25, 2)) < pow(0.1, 2) ? 1 : 0', degree = 1)
-  I2 = Expression('(pow(x[0] + 2, 2) + pow(x[1] + 0.75, 2)) < pow(0.1, 2) ? 1 : 0', degree = 1)
-  I3 = Expression('(pow(x[0] + 2, 2) + pow(x[1] - 0.75, 2)) < pow(0.1, 2) ? 1 : 0', degree = 1)
-  I4 = Expression('(pow(x[0] + 2, 2) + pow(x[1] - 2.25, 2)) < pow(0.1, 2) ? 1 : 0', degree = 1)
+  I1 = Expression('(pow(x[0] + 2, 2) + pow(x[1] + 2.25, 2)) < pow(0.2, 2) ? 1 : 0', degree = 0)
+  I2 = Expression('(pow(x[0] + 2, 2) + pow(x[1] + 0.75, 2)) < pow(0.2, 2) ? 1 : 0', degree = 0)
+  I3 = Expression('(pow(x[0] + 2, 2) + pow(x[1] - 0.75, 2)) < pow(0.2, 2) ? 1 : 0', degree = 0)
+  I4 = Expression('(pow(x[0] + 2, 2) + pow(x[1] - 2.25, 2)) < pow(0.2, 2) ? 1 : 0', degree = 0)
 
   B = Constant((np.cos(theta), np.sin(theta)))
   x0 = Constant(-2.0)
@@ -26,8 +27,6 @@ def FOMsolver(d1, d2, d3, d4, theta):
   sigma_xy = Expression('0.12 * rad * pow(1 + 0.00025 * rad, -0.5)', rad = rad, degree = 1)
   mu = Expression('sigma_xy * sigma_xy * sqrt(pow(B[0], 2) + pow(B[1], 2))/(DOLFIN_EPS + 2 * rad)', sigma_xy = sigma_xy, rad = rad, B = B, degree = 1)
 
-  target = Expression('(pow(x[0] - 3, 2) + pow(x[1] - 0.75, 2)) < pow(0.95, 2) ? 1 : 0', degree = 1)
-
   edge = 'near(x[0], -5)'
   bc_d = DirichletBC(V, Constant(0.0), edge)
 
@@ -36,6 +35,8 @@ def FOMsolver(d1, d2, d3, d4, theta):
   F = inner(mu * grad(u), grad(v)) * dx + inner(B, grad(u)) * v * dx - (w1 * v * I1 * dx + w2 * v * I2 * dx + w3 * v * I3 * dx + w4 * v * I4 * dx)
   solve(F == 0, u, bc_d)
   clc()
+  from fenics import project
+  u = project(u, FOMspace)
   u = u.vector()[:]
   return u*(u>0)
 
